cfa0b3cceab240ea9a106dffcc990cb5701e7ff4
redlarvaoioi <maverickfia29@outlook.com>
2025-02-12 16:45:49 -0800

    Create launcher.sh

---
+++ launcher/launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script runs the .securlybypass file
+
+# Make sure the .securlybypass file is executable
+chmod +x launcher.securlybypass
+
+# Run the .securlybypass file
+./launcher.securlybypass
