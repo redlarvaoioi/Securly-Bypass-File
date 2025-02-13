42c17f3a7418ef8a2f0c7c84130c92495613f0cd
redlarvaoioi <maverickfia29@outlook.com>
2025-02-12 16:28:06 -0800

    Create run.sh
    runs the .securlybypass file

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script runs the .securlybypass file
+
+# Make sure the .securlybypass file is executable
+chmod +x index.securlybypass
+
+# Run the .securlybypass file
+./index.securlybypass
